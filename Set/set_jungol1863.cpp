3a63964df08d8ebb045339d2f6e18ee24eec285f
dirigent-JHK <dirigent74@gmail.com>
2021-03-13 11:29:04 +0900

    Find set example

---
+++ Set/set_jungol1863.cpp
@@ -0,0 +1,57 @@
+/*
+Created by Jeonghan Kim
+
+Set construction by path compressing
+O(log*(n)) ~= O(1)
+Jungol 1863
+*/
+#include <iostream>
+using namespace std;
+
+#define rt register
+#define rint rt int
+
+#define f(i,a,b) for(rint i=a;i<b;++i)
+#define fe(i,a,b) for(rint i=a;i<=b;++i)
+
+#define PATH_COMPRESSION
+
+const int maxStudents = 50000+1;
+
+int N, M, grp[maxStudents];
+
+int maxReligions;
+#ifdef PATH_COMPRESSION
+int find(int k) {
+	if (grp[k] == k) return k;
+	return grp[k] = find(grp[k]);
+}
+#endif
+
+int main() {
+	freopen("input1863.txt", "r", stdin);
+	scanf("%d %d", &N, &M);
+	maxReligions = N;
+
+	fe(i, 1, N) grp[i] = i;
+
+	int u, v;
+	f(i, 0, M) {
+		scanf("%d %d", &u, &v);
+#ifdef PATH_COMPRESSION
+		u = find(u), v = find(v);
+		if (u == v) continue;
+		grp[v] = u;
+#else
+		u = grp[u];
+		v = grp[v];
+
+		if (u == v) continue;
+		fe(j, 1, N) {if (grp[j] == v) grp[j] = u;}
+#endif
+		maxReligions--;
+		
+	}
+	printf("%d\n", maxReligions);
+	return 0;
+}
